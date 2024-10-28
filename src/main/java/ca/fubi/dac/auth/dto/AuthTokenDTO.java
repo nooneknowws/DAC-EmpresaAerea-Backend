c6f71006013534ad90e6e7a55d9988fc8e7c7a73
Thalyson <61944196+nooneknowws@users.noreply.github.com>
2024-10-28 18:02:15 -0300

    Limpa do projeto

--- src/main/java/ca/fubi/dac/auth/dto/AuthTokenDTO.java
+++
@@ -1,6 +0,0 @@
-package ca.fubi.dac.auth.dto;
-
-import ca.fubi.dac.user.UserDTO;
-
-public record AuthTokenDTO(String token, UserDTO user) {
-}
--- src/main/java/ca/fubi/dac/auth/dto/LoginUserDTO.java
+++
@@ -1,9 +0,0 @@
-package ca.fubi.dac.auth.dto;
-
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
-
-public record LoginUserDTO(
-		@NotBlank @Size(max = 64) String email, 
-		@NotBlank @Size(max = 128) String senha) {	
-}
