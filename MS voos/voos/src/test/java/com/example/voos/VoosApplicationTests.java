32d72d5196bc2ada5f57550165cd42f14dd6da14
Victor Hugo <victor.030farias@gmail.com>
2024-10-31 05:27:13 -0300

    build: :tada: Criação do projeto e implementações iniciais

---
+++ MS voos/voos/src/test/java/com/example/voos/VoosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.voos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
