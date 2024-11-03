024f69101d29f946365fd2c340d0e059808a0a76
Victor Hugo <victor.030farias@gmail.com>
2024-11-03 19:59:34 -0300

    feat: :sparkles: Adicionar rota inserir voo

--- MS voos/voos/src/main/java/br/ufpr/dac/voos/rest/VooRest.java
+++ MS voos/voos/src/main/java/br/ufpr/dac/voos/rest/VooRest.java
@@ -1,36 +1,56 @@
 package br.ufpr.dac.voos.rest;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.time.LocalDateTime;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import br.ufpr.dac.voos.models.Voo;
 import br.ufpr.dac.voos.models.Aeroporto;
 
+
 @CrossOrigin
 @RestController
 public class VooRest {
-    public static final List<Voo> voos = new ArrayList<>();
+    public static final List<Voo> listaVoos = new ArrayList<>();
     
     @GetMapping("/voos")
-    public List<Voo> getVoos() {
-        return voos;
+    public List<Voo> listarTodosVoos() {
+        return listaVoos;
     }
 
     @GetMapping("/voos/{id}")
-    public Voo getVoo(@PathVariable("id") String id) {
-        return voos.stream().filter(v -> v.getId().equals(id)).findAny().orElse(null);
+    public Voo listarUmVoo(@PathVariable("id") String id) {
+        return listaVoos.stream().filter(v -> v.getId().equals(id)).findAny().orElse(null);
+    }
+
+    @PostMapping("/voos")
+    public Voo inserirVoo(@RequestBody Voo voo) {
+        
+        Voo novoVoo = listaVoos.stream().max(Comparator.comparing(Voo::getId)).orElse(null);
+
+        if (novoVoo == null) {
+            voo.setId("1");
+        } else {
+            voo.setId(novoVoo.getId() + 1);
+            listaVoos.add(voo);
+        }
+        
+        return voo;
     }
     
 
     static {
-        voos.add(new Voo("1", "Voo 1", LocalDateTime.now(), new Aeroporto("1", "Aeroporto 1", "Cidade 1", "Estado 1", "Pais 1"), new Aeroporto("2", "Aeroporto 2", "Cidade 2", "Estado 2", "Pais 2"), 100.0, 100, 0, "Ativo"));
-        voos.add(new Voo("2", "Voo 2", LocalDateTime.now(), new Aeroporto("3", "Aeroporto 3", "Cidade 3", "Estado 3", "Pais 3"), new Aeroporto("4", "Aeroporto 4", "Cidade 4", "Estado 4", "Pais 4"), 100.0, 100, 0, "Ativo"));
-        voos.add(new Voo("3", "Voo 3", LocalDateTime.now(), new Aeroporto("5", "Aeroporto 5", "Cidade 5", "Estado 5", "Pais 5"), new Aeroporto("6", "Aeroporto 6", "Cidade 6", "Estado 6", "Pais 6"), 100.0, 100, 0, "Ativo"));
+        listaVoos.add(new Voo("1", "Voo 1", LocalDateTime.now(), new Aeroporto("1", "Aeroporto 1", "Cidade 1", "Estado 1", "Pais 1"), new Aeroporto("2", "Aeroporto 2", "Cidade 2", "Estado 2", "Pais 2"), 100.0, 100, 0, "Ativo"));
+        listaVoos.add(new Voo("2", "Voo 2", LocalDateTime.now(), new Aeroporto("3", "Aeroporto 3", "Cidade 3", "Estado 3", "Pais 3"), new Aeroporto("4", "Aeroporto 4", "Cidade 4", "Estado 4", "Pais 4"), 100.0, 100, 0, "Ativo"));
+        listaVoos.add(new Voo("3", "Voo 3", LocalDateTime.now(), new Aeroporto("5", "Aeroporto 5", "Cidade 5", "Estado 5", "Pais 5"), new Aeroporto("6", "Aeroporto 6", "Cidade 6", "Estado 6", "Pais 6"), 100.0, 100, 0, "Ativo"));
+        //listaVoos.add(new Voo("4", "Voo 4", LocalDateTime.now(), new Aeroporto("7", "Aeroporto 7", "Cidade 7", "Estado 7", "Pais 7"), new Aeroporto("8", "Aeroporto 8", "Cidade 8", "Estado 8", "Pais 8"), 100.0, 100, 0, "Ativo"));
     }
 
 
